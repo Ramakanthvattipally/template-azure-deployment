3589c4c24aa4d2bf470de54f1c103a28378dbcbe
Ramakanth <ramakanthvattipally@gmail.com>
2023-06-04 15:45:23 +0100

    commiting changes for terraform template

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "example" {
   name                     = "ramterraformstorage04"
-  resource_group_name      = "-Template-deployment1"
+  resource_group_name      = "Template-deployment1"
   location                 = "westeurope"
   account_tier             = "Standard"
   account_replication_type = "GRS"
